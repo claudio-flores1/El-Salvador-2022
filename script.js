63260c1087667ce2482fb4cbe7e822cbfb23fc89
Claudio Flores <claudioflrs1@gmail.com>
2022-03-05 19:41:07 -0500

    Added photos and captions, changed font-family

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "El Salvador through the lense of a native";
 document.querySelector("body").appendChild(h2);
 const h3 = document.createElement("h3");
-h3.textContent = "Welcome to El Salvador in 2022!";
+h3.textContent = "In development";
 document.querySelector("body").appendChild(h3);
